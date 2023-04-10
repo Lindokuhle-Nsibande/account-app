e1b61685beab8e9f35dcde38cc37544cac96be54
Lindokuhle Nsibande <lindokuhleascar@gmail.com>
2023-04-10 12:12:18 +0200

    added register livewire

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -7,20 +7,23 @@
         <div class="card-container py-5 px-7  bg-white rounded-xl w-400 text-center text-gray-500">
             <h1 class="text-4xl text-gray-700 font-bold mb-2">Welcome Back</h1>
             <p class="mb-5">Lorem ipsum dolor sit amet, consectetur adipiscing elit </p>
-
-            <div class="grid">
-                <div class="input-container col-span-12 mb-2 text-left">
-                    <label class="" for="email">Email</label>
-                    <input class="w-full px-4 py-3 rounded-md ring-1 ring-inset ring-gray-300" id="email" name="email" type="email" placeholder="Email">
+    
+            <form action="" wire:submit = "login">
+                <div class="grid">
+                    <div class="input-container col-span-12 mb-2 text-left">
+                        <label class="" for="email">Email</label>
+                        <input class="w-full px-4 py-3 rounded-md ring-1 ring-inset ring-gray-300" id="email" name="email" type="email" placeholder="Email">
+                    </div>
+                    <div class="input-container col-span-12 mb-2 text-left">
+                        <label class="" for="password">Password</label>
+                        <input class="w-full px-4 py-3 rounded-md ring-1 ring-inset ring-gray-300" id="password" name="password" type="password" placeholder="Password">
+                    </div>
+                    <a class="col-end-13 col-span-2" href="">Forgot password?</a>
                 </div>
-                <div class="input-container col-span-12 mb-2 text-left">
-                    <label class="" for="password">Password</label>
-                    <input class="w-full px-4 py-3 rounded-md ring-1 ring-inset ring-gray-300" id="password" name="password" type="password" placeholder="Password">
-                </div>
-                <a class="col-end-13 col-span-2" href="">Forgot password?</a>
-            </div>
+            </form>
             <button class="rounded-xl bg-primary py-3 w-full text-white font-bold mt-5 hover:bg-primary-light">Register</button>
             <p class="mt-5">Don't have an account? <a href="/register" class="text-gray-700 font-bold">Register</a></p>
         </div>
     </section>
+    
 @endsection
